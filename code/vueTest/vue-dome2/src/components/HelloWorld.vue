315aff9e5e07c8c488c66ad87dd26418d9ad0413
“1287411583” <1287411583@qq.com>
2023-04-30 04:06:28 +0800

    11

---
+++ code/vueTest/vue-dome2/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+    <h3>Installed CLI Plugins11</h3>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+  data() {
+    return {
+      msg:"vue今天学完"
+    }
+  },
+}
+</script>
+
